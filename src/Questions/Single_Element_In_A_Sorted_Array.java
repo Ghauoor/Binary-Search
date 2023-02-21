548dac9d0f84e5ff45aed5f817acb5af7506cb33
Ghayoor <ghayoorhussain701@gmail.com>
2023-02-21 13:20:06 +0500

    You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once. Return the single element that appears only once.

---
+++ src/Questions/Single_Element_In_A_Sorted_Array.java
@@ -0,0 +1,22 @@
+package Questions;
+
+public class Single_Element_In_A_Sorted_Array {
+    private int singleNonDuplicate(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (mid % 2 == 1) mid--;
+
+            if (arr[mid] != arr[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid + 2;
+            }
+        }
+
+        return arr[start];
+    }
+}
